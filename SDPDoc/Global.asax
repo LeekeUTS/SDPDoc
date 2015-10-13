9cb00b7942501891f1cd0ff9e9ee7156b504ec61
Leeke <leeke.zhou@gmail.com>
2015-10-13 20:35:05 +1100

    1st

---
+++ SDPDoc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SDPDoc.MvcApplication" Language="C#" %>
---
+++ SDPDoc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SDPDoc.Startup))]
+namespace SDPDoc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
